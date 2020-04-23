bd6e5fced09644ce0470626672e4b31f911d6e76
Huseyin Yuceer <hsyn.yuceer44@gmail.com>
2020-04-23 16:00:24 -0400

    add HelloGitWorld.java

---
+++ src/git/Conditional_Operators.java
@@ -0,0 +1,8 @@
+package git;
+
+public class Conditional_Operators {
+    public static void main(String[] args) {
+        int num=55;
+        System.out.println(num>50 ?"youre old" :"youre young");
+    }
+}
---
+++ src/test/HelloGitWorld.java
@@ -0,0 +1,7 @@
+package test;
+
+public class HelloGitWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello dear code");
+    }
+}
