42367b6bd8ff8acd3a40d45aee1b1a980cc1c87d
Huseyin Yuceer <hsyn.yuceer44@gmail.com>
2020-04-24 15:42:19 -0400

    updates

--- src/test/Conditional_Operators.java
+++ src/test/Conditional_Operators.java
@@ -4,5 +4,8 @@ public class Conditional_Operators {
     public static void main(String[] args) {
         int num=55;
         System.out.println(num>50 ?"youre old" :"youre young");
+
+        System.out.println("fdfdsfdsfds");
+
     }
 }
